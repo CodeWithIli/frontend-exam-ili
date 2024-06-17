a71b3be2038f8691f23c9abe101cfe20acd3c69f
Ili Aliaj <ilialiaj3@gmail.com>
2024-06-17 10:52:08 +0200

    changes made based on comments

--- ili_main_react_components/src/pages/App.jsx
+++ ili_main_react_components/src/pages/App.jsx
@@ -1,19 +1,10 @@
 import React from "react";
-<<<<<<< HEAD:ili_main_react_components/src/pages/App.jsx
-import Header from "../components/Header";
-import PhotoFunc from "../components/PhotoFunc";
-=======
-import Header from "./components/UI/Header";
-import PhotoGallery from "./components/UI/PhotoGallery";
->>>>>>> ili_main_react_components:ili_main_react_components/src/App.jsx
+import HomePage from "./HomePage";
 
 function App() {
 
   return (
-    <main className="grid justify-center">
-        <Header />
-        <PhotoGallery />
-    </main>
+    <HomePage/>
   );
 }
 
