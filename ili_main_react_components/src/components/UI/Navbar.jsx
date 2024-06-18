0f3e9668108681d6d10fb67d499da54501d70e43
Ili Aliaj <ilialiaj3@gmail.com>
2024-06-18 15:39:21 +0200

    implemented useRoutes

--- ili_main_react_components/src/components/UI/Navbar.jsx
+++ ili_main_react_components/src/components/UI/Navbar.jsx
@@ -1,16 +1,13 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className='border-2 border-black p-2 m-3 rounded-lg min-w-[930px] flex justify-evenly'>
-        <Link to="/">Welcome</Link>
-        <Link to="/home/home-page-1">Home 1</Link>
-        <Link to="/home/home-page-2">Home 2</Link>
-        <Link to="/test/test-page">Test</Link>
+    <nav className="border-2 border-black p-2 m-3 rounded-lg min-w-[930px] flex justify-evenly">
+      <Link to="/">Welcome</Link>
+      <Link to="/home/">Home</Link>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
